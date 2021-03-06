e8038c76aa1e62660b27a36cd910a5dba8d0a458
constantinsever <constantinsever@gmail.com>
2021-03-07 00:36:08 +0100

    corrected routes

--- ccdemo.py
+++ ccdemo.py
@@ -1,4 +1,4 @@
-aaaimport flask
+import flask
 from flask import request, jsonify, render_template
 import boto3
 
@@ -6,19 +6,24 @@ app = flask.Flask(__name__)
 
 app.config["DEBUG"] = True
 
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'}
-]
-
-
-
-@app.route('/')
+@app.route('/index')
 def index():
    return render_template('index.html')
+
+@app.route('/vms')
+def vms():
+   return render_template('vms.html')
+
+@app.route('/users')
+def users():
+   return render_template('users.html')
+
+@app.route('/inventory')
+def inventory():
+   return render_template('inventory.html')
+
+
+
 
 
 app.run(host='0.0.0.0', port=80)
